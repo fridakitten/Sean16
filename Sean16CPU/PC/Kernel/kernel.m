97f8bf05afedec695e54cfcb81ceb32ff7dd0aff
FridaDEV <157828245+fridakitten@users.noreply.github.com>
2024-10-01 19:32:29 +0200

    Peripherals => Mouse Buttons mapped?

--- Sean16CPU/PC/Kernel/kernel.m
+++ Sean16CPU/PC/Kernel/kernel.m
@@ -14,24 +14,20 @@ extern void *execute(void *arg);
 
 void kernel_init(uint8_t binmap[1000][6]) {
     // INIT
-    uint8_t kernelmap[1000][6];
-    
     CursorTracker *mouse = [[CursorTracker alloc] init];
     [mouse startTracking];
     
     // fork process
-    proc *kernel_task = proc_fork(kernelmap);
-    kernel_task->peri = genpage();
-    *((CGPoint **)&kernel_task->peri->memory[0][0]) = [mouse getCursorPosition];
-    
     proc *child_task = proc_fork(binmap);
     child_task->peri = genpage();
     *((CGPoint **)&child_task->peri->memory[0][0]) = [mouse getCursorPosition];
+    *((NSInteger **)&child_task->peri->memory[0][1]) = [mouse getLastMouseButtonState];
     
     /*if (pthread_create(&child_task->thread, NULL, execute, (void *)child_task) != 0) {
         perror("Failed to create thread");
     }
     pthread_join(child_task->thread, NULL);*/
+    
     execute((void*)child_task);
 
     // DEINIT
@@ -39,5 +35,4 @@ void kernel_init(uint8_t binmap[1000][6]) {
     
     // killing task
     proc_kill(child_task);
-    proc_kill(kernel_task);
 }
