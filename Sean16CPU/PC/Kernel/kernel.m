c3aea104c31b7977b10744bda004dfb0b00f8d5e
FridaDEV <157828245+fridakitten@users.noreply.github.com>
2024-10-01 12:57:19 +0200

    tracking mouse

--- Sean16CPU/PC/Kernel/kernel.m
+++ Sean16CPU/PC/Kernel/kernel.m
@@ -15,11 +15,11 @@ extern void *execute(void *arg);
 void kernel_init(uint8_t binmap[1000][6]) {
     uint8_t kernelmap[1000][6];
     
+    CursorTracker *mouse = [[CursorTracker alloc] init];
+    [mouse startTracking];
+    
     // periphal memory mapping
     page_t * periphals = genpage();
-    CursorTracker *mouse = [[CursorTracker alloc] init];
-    
-    //mapping mouse to memory
     *((CGPoint **)&periphals->memory[0][0]) = [mouse getCursorPosition];
     
     // fork process
@@ -29,8 +29,6 @@ void kernel_init(uint8_t binmap[1000][6]) {
     if (pthread_create(&child_task->thread, NULL, execute, (void *)child_task) != 0) {
         perror("Failed to create thread");
     }
-
-    // Wait for the thread to finish
     pthread_join(child_task->thread, NULL);
 
     // killing task
