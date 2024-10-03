5a8c47fff72d003c2298d52d3d16bc715e2980cb
FridaDEV <157828245+fridakitten@users.noreply.github.com>
2024-10-03 17:38:01 +0200

    CPU => rdrand fix

--- Sean16CPU/PC/CPU/rdrand.c
+++ Sean16CPU/PC/CPU/rdrand.c
@@ -6,16 +6,14 @@
 //
 
 #include "rdrand.h"
+#include <stdlib.h>
 
 void rdrand(uint16_t *ptr, uint16_t min, uint16_t max) {
-    // Ensure that min is less than or equal to max
     if (min > max) {
         uint16_t temp = min;
         min = max;
         max = temp;
     }
-
-    // Generate a random value in the range [min, max]
-    uint16_t rand_val = (rand() % (max - min + 1)) + min;
+    uint16_t rand_val = arc4random_uniform(max - min + 1) + min;
     *ptr = rand_val;
 }
