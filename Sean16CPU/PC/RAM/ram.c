7c31df872251848c5c811726437828459b0850b3
FridaDEV <157828245+fridakitten@users.noreply.github.com>
2024-10-03 14:31:23 +0200

    RAM => generating same page fixed

--- Sean16CPU/PC/RAM/ram.c
+++ Sean16CPU/PC/RAM/ram.c
@@ -9,6 +9,7 @@
 #include "ram.h"
 
 static uint8_t RAMDISK[S_RAMSIZE_MAX];
+static uint16_t current_addr = 0;
 
 page_t* genpage(void) {
     page_t *page = malloc(sizeof(page_t));
@@ -18,10 +19,12 @@ page_t* genpage(void) {
     }
 
     for (int i = 0; i < 6; i++) {
-        for (int j = 0; j < 1024; j++) {
+        for (int j = current_addr + 0; j < current_addr + 1024; j++) {
             page->memory[i][j] = &RAMDISK[i * 1024 + j];
         }
     }
+    
+    current_addr += 1024;
 
     return page;
 }
