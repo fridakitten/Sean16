f47bcf001e4a933978d0772896d117cc29f767ab
FridaDEV <157828245+fridakitten@users.noreply.github.com>
2024-10-04 17:10:55 +0200

    more logs

--- Sean16CPU/PC/Kernel/kernel.m
+++ Sean16CPU/PC/Kernel/kernel.m
@@ -14,28 +14,37 @@
 extern void *execute(void *arg);
 
 void kernel_init(uint8_t binmap[1000][6], NSWindow *window) {
+    printf("[soc-bootloader] clearing screen\n");
     clearScreen();
     
     // INIT
+    printf("[soc-bootloader] initialising mouse\n");
     CursorTracker *mouse = [[CursorTracker alloc] initWithWindow:window];
     [mouse startTracking];
     
     // fork process
+    printf("[soc-bootloader] forking kernel process\n");
     proc *child_task = proc_fork(binmap);
     child_task->peri = genpage();
     
     // peripherials mapping
+    printf("[soc-bootloader] mapping peripherals\n");
     *((CGPoint **)&child_task->peri->memory[0][0]) = [mouse getCursorPosition];
     *((NSInteger **)&child_task->peri->memory[0][1]) = [mouse getLastMouseButtonState];
     
     // executing process
+    printf("[soc-bootloader] executing kernel process\n");
     execute((void*)child_task);
 
     // DEINIT
+    printf("[soc-bootloader] process has finished execution\n");
+    printf("[soc-bootloader] deinitialising mouse\n");
     [mouse stopTracking];
     
     // killing task
+    printf("[soc-bootloader] freeing process\n");
     proc_kill(child_task);
     
+    printf("[soc-bootloader] clearing screen\n");
     clearScreen();
 }
